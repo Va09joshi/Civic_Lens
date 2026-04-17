29af8c09ae0bc81abd0e2fcee46442c8175aebe3
vaibhav joshi <vaibhavjosh0709@gmail.com>
2026-04-17 15:26:36 +0530

    Rate limiter

--- backend/src/middleware/rateLimit.middleware.js
+++ backend/src/middleware/rateLimit.middleware.js
@@ -1,16 +1,18 @@
 const rateLimit = require('express-rate-limit');
 
+
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // 100 requests per windowMs
+  max: 300, // 300 requests per windowMs
   message: 'Too many requests, please try again later',
   standardHeaders: true,
   legacyHeaders: false,
 });
 
+
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // 5 requests per windowMs
+  max: 15, // 15 requests per windowMs
   message: 'Too many login attempts, please try again later',
   standardHeaders: true,
   legacyHeaders: false,
